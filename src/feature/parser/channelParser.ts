b2142fb5f46cef50ebe064c3d4d56f89d2307b52
Jaemin <ghdckdwn8456@gmail.com>
2026-04-14 10:14:50 +0900

    [feat] channel id 파싱 로직 개편

--- src/feature/parser/channelParser.ts
+++ src/feature/parser/channelParser.ts
@@ -3,38 +3,38 @@ import Channel from "../../domain/channel";
 class ChannelParser {
 
     parseChannel(informationDetail: Element): Channel {
-        const id = this.parseChannelId();
+        const id = this.parseChannelId(informationDetail);
         const name = this.parseChannelName(informationDetail);
         const profileUrl = this.parseChannelProfileUrl(informationDetail);
         return new Channel(id, name, profileUrl);
     }
 
-    private parseChannelId(): string {
-        const url = location.href;
-        const id = url.match(/\/live\/([^\/?]+)/);
-        if (!id) {
-            throw new Error("Channel ID not found in URL");
+    private parseChannelId(informationDetail: Element): string {
+        const root = informationDetail.querySelector('div[class^="video_information_name"]');
+        const href = root?.querySelector('a[class^="video_information_link"]')?.getAttribute('href');
+        if (!root || !href) {
+            throw new Error("Channel ID not found");
         }
 
-        return id[1];
+        return href.replace(/\//g, '').trim();
     }
 
     private parseChannelName(informationDetail: Element): string {
-        const nameEl = informationDetail.querySelector('span[class^="name_text"]');
-        if (!nameEl) {
+        const name = informationDetail.querySelector('span[class^="name_text"]');
+        if (!name || !name.textContent) {
             throw new Error("Channel name element not found");
         }
 
-        return nameEl?.textContent?.trim() || "";
+        return name.textContent;
     }
 
     private parseChannelProfileUrl(informationDetail: Element): string {
-        const channelProfileEl = informationDetail.querySelector<HTMLImageElement>('a[class^="video_information_thumbnail"] img');
-        if (!channelProfileEl) {
+        const channelProfile = informationDetail.querySelector<HTMLImageElement>('a[class^="video_information_thumbnail"] img');
+        if (!channelProfile) {
             throw new Error("Channel profile element not found");
         }
 
-        return channelProfileEl.src;
+        return channelProfile.src;
     }
 }
 
