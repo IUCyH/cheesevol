babd5fda5e50fddfe2a3db60d6a205d05545d550
Jaemin <ghdckdwn8456@gmail.com>
2026-04-14 12:54:01 +0900

    [feat] 생방송 채널에 들어왔을 때만 초기화 되도록 개선

--- src/entry/content/content.ts
+++ src/entry/content/content.ts
@@ -2,6 +2,7 @@ import channelParser from "../../feature/parser/channelParser";
 import rootElementParser from "../../feature/parser/rootElementParser";
 import volumeManager from "../../feature/volume/volumeManager";
 
+const livePath = "chzzk.naver.com/live";
 let isInitialized = false;
 let currentUrl = location.href;
 
@@ -14,12 +15,17 @@ async function init() {
 }
 if (!isInitialized) {
     isInitialized = true;
+    currentUrl = location.href; // 혹시 모를 버그 방지를 위해 명시적으로 다시 초기화
 
-    await init(); // 첫 로드 시 앱 초기화
+    if (currentUrl.includes(livePath)) {
+        await init(); // 첫 로드 시 앱 초기화
+    }
+
     // SPA 대응, 페이지가 변경되지 않더라도 URL이 변경된다면 다시 앱 초기화
     window.navigation.addEventListener("navigate", async (event) => {
-        if (event.destination.url !== currentUrl) {
-            currentUrl = event.destination.url;
+        const destUrl = event.destination.url;
+        if (destUrl !== currentUrl && destUrl.includes(livePath)) {
+            currentUrl = destUrl;
             await init();
         }
     });
